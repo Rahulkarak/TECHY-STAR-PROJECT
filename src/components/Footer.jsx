229da9b6f970ce7592903f3225bcccbb48ce95c2
rahul <rahulkarak808@gmail.com>
2024-02-03 16:57:16 +0530

    ujhh

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return <footer>
+    <div>
+      <h1>Techystar</h1>  
+      <p>@all right reversed</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="http://youtube.com" target={"blank"}>Youtube</a>
+            <a href="http://instagram.com" target={"blank"}>Instagram</a>
+            <a href="http://github.com" target={"blank"}>Github</a>
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer
